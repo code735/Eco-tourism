fc8ed6d1d00485a3185677898006faaa737b774c
Amarjeet Ray <amarjeetray6515@gmail.com>
2023-05-24 19:12:22 +0530

    merge commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Payment from "./components/Payment";
+
 
 function App() {
   return (
     <div className="App">
-      <Payment />
+
     </div>
   );
 }
---
+++ src/components/Camping/Camping.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Box,Heading,SimpleGrid,Text} from '@chakra-ui/react';
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+const Camping = () => {
+    const [camping, setCamping] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/camping")
+            .then((res) => {
+                setCamping(res.data);
+            })
+    }, [])
+  return (
+   <Box style={{backgroundColor:"#eaeded"}}>
+        <Heading as='h4' size='md' style={{marginLeft:"-1100px"}}>
+                CAMPING PACKAGE
+            </Heading>&nbsp;&nbsp;&nbsp;&nbsp;
+
+            <SimpleGrid columns={3} spacing={10}>
+      
+            {
+                camping.length > 0 && camping.map((ele) => {
+                    return (
+                        <Box  key={ele.id} style={{backgroundColor:"whitesmoke"}} borderWidth='1px' borderColor='gray.200' >
+                            <img src={ele.image} alt={ele.title} />
+                            <Box className='right-box'>
+                                <Heading as='h3' size='md' >{ele.title}</Heading>
+                                <Text fontSize='md'>{ele.about}</Text>
+                                <Heading as='h3' size='sm' style={{lineHeight:"3",margin:"5px"}} >From ₹{ele.price}/per person</Heading>
+                            </Box>
+                        </Box>
+                    )
+                })
+            }
+            </SimpleGrid>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;
+
+   </Box>
+  )
+}
+
+export default Camping
