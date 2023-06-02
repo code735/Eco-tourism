843797180af8b3a09c99140007679fd4815eaeb7
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-06-02 22:18:20 +0530

    searchBox

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -22,13 +22,16 @@ const City = () => {
         <Box 
         style={{gap:"20px"}} 
         display={'grid'} 
-        gridTemplateColumns={'repeat(3,1fr)'} 
+        gridTemplateColumns={'repeat(2,1fr)'} 
         color={'white'}
         padding={'5%'}
-        width={'78%'}
+        width={'50%'}
         margin={'auto'}
         marginTop={'5%'}
         paddingBottom={0}
+        overflowY={'scroll'}
+        className='explore-dest'
+        height={'80vh'}
         >
             {
                 state.map((ele,index) => {
---
+++ src/components/FilterBox.jsx
@@ -0,0 +1,10 @@
+import { Box, Input } from '@chakra-ui/react'
+import React from 'react'
+
+export default function FilterBox() {
+  return (
+    <Box width={'45%'}>
+        <Input color={'white'}/>
+    </Box>
+  )
+}
