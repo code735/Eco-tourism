d5039d6f412463015077b30e40b43d45feabd118
Prathmesh Kumbhar <prathmeshjkumbhar@gmail.com>
2023-07-08 16:51:49 +0530

    !

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -5,7 +5,6 @@ import { loginFailure, loginSuccess } from '../Redux/action';
 import { Navigate, useNavigate } from 'react-router-dom';
 import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 import jwtDecode from 'jwt-decode';
-import { Preloader } from '../components/Preloader';
 import swan from '../Images/swan.jpg'
 import loginLogo from '../Images/login-page-logo.png'
 
@@ -46,7 +45,7 @@ export default function Login() {
   useEffect(() => {
     const storedAuth = localStorage.getItem('isAuthenticated');
     if (storedAuth === 'true') {
-      dispatch(loginSuccess()); // Assuming you don't need to pass user_obj here
+      dispatch(loginSuccess()); 
     }
   }, []);
 
@@ -68,78 +67,75 @@ export default function Login() {
           overflow: 'hidden',
         }}
       >
-        {/* Rest of the code */
+        
           <Flex className="signin-and-heading-text-flex-box">
-          <Box
-            width={'50%'}
-            height={'100vh'}
-            display={'flex'}
-            flexDirection={'column'}
-            alignItems={'center'}
-            justifyContent={'space-around'}
-          >
-            <Image src={loginLogo} w={'250px'} />
-            <Flex textAlign={'center'} flexDirection={'column'}  gap={'20px'}>
-              <GoogleLogin
-                onSuccess={handleCredentialResponse}
-                theme='outline'
-                shape='square'
-                size='medium'
-                logo_alignment='center'
-                width='200px'
-                onError={() => {
-                  console.log('Login Failed');
+            <Box
+              width={'50%'}
+              height={'100vh'}
+              display={'flex'}
+              flexDirection={'column'}
+              alignItems={'center'}
+              justifyContent={'space-around'}
+            >
+              <Image src={loginLogo} w={'250px'} />
+              <Flex textAlign={'center'} flexDirection={'column'}  gap={'20px'}>
+                <GoogleLogin
+                  onSuccess={handleCredentialResponse}
+                  theme='outline'
+                  shape='square'
+                  size='medium'
+                  logo_alignment='center'
+                  width='200px'
+                  onError={() => {
+                    console.log('Login Failed');
+                  }}
+                  text='continue_with'
+                />
+                <Button 
+                className="back-to-home-btn" 
+                height={'35px'} 
+                border={'2px solid white'} 
+                background={'transparent'} 
+                color={'white'}
+                _hover={{
+                  color:"black",
+                  background:"white"
                 }}
-                text='continue_with'
-              />
-              <Button 
-              className="back-to-home-btn" 
-              height={'35px'} 
-              border={'2px solid white'} 
-              background={'transparent'} 
-              color={'white'}
-              _hover={{
-                color:"black",
-                background:"white"
-              }}
-              fontSize={'.9rem'}
-              onClick={()=>{
-                navigate('/');           
-              }}
-              >Back To Home</Button>
-            </Flex>
-          </Box>
-          <Box width={'50%'} textAlign={'left'} color={'white'} className="signin-box-text">
-            <h4
-              style={{
-                fontFamily: 'Italiana, serif',
-                fontWeight: '100',
-                fontSize: '3.2rem',
-                width: '49%',
-                display: 'flex',
-              }}
-              className="login-heading"
-            >
-              Login to an Authentic eco-tourism Experience
-            </h4>
-            <h5
-              style={{
-                fontFamily: 'Estonia, cursive',
-                fontWeight: '100',
-                fontSize: '2.3rem',
-                width: '85%',
-              }}
-              className="login-para"
-            >
-              Step into the enchanting world of eco-tourism and embrace the rustic charm of authentic experiences.
-              Immerse yourself in the beauty of nature as you embark on a sustainable journey.
-            </h5>
-          </Box>
+                fontSize={'.9rem'}
+                onClick={()=>{
+                  navigate('/');           
+                }}
+                >Back To Home</Button>
+              </Flex>
+            </Box>
+            <Box width={'50%'} textAlign={'left'} color={'white'} className="signin-box-text">
+              <h4
+                style={{
+                  fontFamily: 'Italiana, serif',
+                  fontWeight: '100',
+                  fontSize: '3.2rem',
+                  width: '49%',
+                  display: 'flex',
+                }}
+                className="login-heading"
+              >
+                Login to an Authentic eco-tourism Experience
+              </h4>
+              <h5
+                style={{
+                  fontFamily: 'Estonia, cursive',
+                  fontWeight: '100',
+                  fontSize: '2.3rem',
+                  width: '85%',
+                }}
+                className="login-para"
+              >
+                Step into the enchanting world of eco-tourism and embrace the rustic charm of authentic experiences.
+                Immerse yourself in the beauty of nature as you embark on a sustainable journey.
+              </h5>
+            </Box>
           </Flex>
-        }
       </div>
     </GoogleOAuthProvider>
   );
 }
-
-
